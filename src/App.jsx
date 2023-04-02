788f6f72d57027ab2e45632ffc306f133e1491f8
Durjoy-dcp <unknownccxv@gmail.com>
2023-04-02 22:27:03 +0600

    picture fatching test from firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { storage } from "./Firebase/firebase.init";
+import { getDownloadURL, ref } from "firebase/storage";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const secret = import.meta.env.VITE_MY_SECRET;
+  const [imgurl, setImage] = useState("");
+  useEffect(() => {
+    const pathRef = ref(storage, "Images/Female01.jpg");
+    getDownloadURL(pathRef).then((url) => {
+      console.log(url);
+      setImage(url);
+    });
+    // console.log(url);
+    // setImage(url);
+  }, []);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <img src={imgurl} alt="hello" />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
