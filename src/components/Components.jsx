0b64fe0ce3799008f4e50cc99f00edb703274fc6
Durjoy-dcp <unknownccxv@gmail.com>
2023-04-03 04:23:57 +0600

    20% layout

--- src/components/Components.jsx
+++ src/components/Components.jsx
@@ -3,10 +3,13 @@ import React from "react";
 const Components = () => {
   return (
     <div>
-      <div className="  border mx-auto border-cyan-300 flex flex-row">
-        <div className="basis-1/12 bg-gray-900">Sidebar</div>
-        <div className="basis-10/12 bg-red-900">Information</div>
-        <div className="basis-1/2 bg-blue-900"> Events</div>
+      <div className="min-h-screen   border mx-auto border-cyan-300 flex flex-row">
+        <div className="basis-1/12 bg-[#5BB8F1]">Sidebar</div>
+        <div className="basis-10/12 bg-white">Information</div>
+        <div className="basis-1/2 bg-white border border-[#D9D9D9] border-8 ">
+          {" "}
+          Events
+        </div>
       </div>
     </div>
   );
